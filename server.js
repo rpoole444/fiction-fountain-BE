4a56d370a8d2007117b030311ded2a5ac0e05986
Reid Poole <poole.reid@gmail.com>
2023-04-18 13:41:46 -0500

    Add Cors stuff

--- server.js
+++ server.js
@@ -7,8 +7,21 @@ const axios = require("axios");
 dotenv.config();
 
 const app = express();
+const allowedOrigins = ["http://localhost:3000"];
 
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 app.use(express.json());
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
